f4b4bcfc818c7455c2619aa65ede2e9d1f8a87e6
Junya Suzuki <suzuki.junya@gmail.com>
2016-09-02 11:25:40 +0900

    capter8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,2 @@
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-
-  config.include FactoryGirl::Syntax::Methods
-
-  config.before(:suite) do
-    FactoryGirl.reload
-  end
 end

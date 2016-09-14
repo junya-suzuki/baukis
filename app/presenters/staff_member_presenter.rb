1e48722837eaa33bfb8de3e8cf4e03b11b2d2c57
Junya Suzuki <suzuki.junya@gmail.com>
2016-09-14 15:15:44 +0900

    chapter16-5

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,8 +1,14 @@
 class StaffMemberPresenter < ModelPresenter
   delegate :suspended?, to: :object
-  # 職員の停止フラグのOn/Offを表現する記号を返す。
-  #   On: BLLOT BOX WITH CHECK (U+2611)
-  #   Off: BLLOT BOX (U+2610)
+
+  def full_name
+    object.family_name + ' ' + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + ' ' + object.given_name_kana
+  end
+
   def suspended_mark
     suspended? ? raw('&#x2611;') : raw('&#x2610;')
   end
